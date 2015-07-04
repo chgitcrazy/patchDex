935d1fb29d76289af73e44e43185046498c4c625
ch <chawxw@gmail.com>
2015-07-04 15:33:45 +0800

    first commit

---
+++ src/patchDex.java
@@ -0,0 +1,103 @@
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.Adler32;
+
+public class patchDex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		File f = new File("src/HelloJni.dex");
+		try {
+			byte[] buffer = readFileBytes(f);
+//			System.out.println(buffer.length);
+			fixFileSizeHeader(buffer);
+			fixSHA1Header(buffer);
+			fixCheckSumHeader(buffer);
+			
+			FileOutputStream localFileOutputStream = new FileOutputStream("src/classes.dex");
+			localFileOutputStream.write(buffer);
+			localFileOutputStream.flush();
+			localFileOutputStream.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void fixCheckSumHeader(byte[] dexBytes) {
+		Adler32 adler = new Adler32();
+		adler.update(dexBytes, 12, dexBytes.length - 12);
+		long value = adler.getValue();
+		int va = (int) value;
+		byte[] newcs = intToByte(va);
+		byte[] recs = new byte[4];
+		for (int i = 0; i < 4; i++) {
+			recs[i] = newcs[newcs.length - 1 - i];
+			System.out.println(Integer.toHexString(newcs[i]));
+		}
+		System.arraycopy(recs, 0, dexBytes, 8, 4);
+		System.out.println(Long.toHexString(value));
+		System.out.println();
+	}
+
+	public static byte[] intToByte(int number) {
+		byte[] b = new byte[4];
+		for (int i = 3; i >= 0; i--) {
+			b[i] = (byte) (number % 256);
+			number >>= 8;
+		}
+		return b;
+	}
+
+	private static void fixSHA1Header(byte[] dexBytes)
+			throws NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.update(dexBytes, 32, dexBytes.length - 32);
+		byte[] newdt = md.digest();
+		System.arraycopy(newdt, 0, dexBytes, 12, 20);
+		String hexstr = "";
+		for (int i = 0; i < newdt.length; i++) {
+			hexstr += Integer.toString((newdt[i] & 0xff) + 0x100, 16)
+					.substring(1);
+		}
+		System.out.println(hexstr);
+	}
+
+	private static void fixFileSizeHeader(byte[] dexBytes) {
+
+		byte[] newfs = intToByte(dexBytes.length);
+		System.out.println(Integer.toHexString(dexBytes.length));
+		byte[] refs = new byte[4];
+		for (int i = 0; i < 4; i++) {
+			refs[i] = newfs[newfs.length - 1 - i];
+			System.out.println(Integer.toHexString(newfs[i]));
+		}
+		System.arraycopy(refs, 0, dexBytes, 32, 4);
+	}
+
+	private static byte[] readFileBytes(File file) throws IOException {
+		byte[] arrayOfByte = new byte[1024];
+		ByteArrayOutputStream localByteArrayOutputStream = new ByteArrayOutputStream();
+		FileInputStream fis = new FileInputStream(file);
+		while (true) {
+			int i = fis.read(arrayOfByte);
+			if (i != -1) {
+				localByteArrayOutputStream.write(arrayOfByte, 0, i);
+			} else {
+				return localByteArrayOutputStream.toByteArray();
+			}
+		}
+	}
+
+}
